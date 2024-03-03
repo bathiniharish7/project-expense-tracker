2be83c3796610b7188a56ef78e93a10ef0d78577
bathiniharish7 <bathiniharish7@gmail.com>
2024-03-03 11:12:06 +0530

    Echarts half completed

--- src/App.js
+++ src/App.js
@@ -6,33 +6,22 @@ import PieChart from './components/PieChart';
 import ListComponent from './components/ListComponent';
 import { useSelector } from 'react-redux';
 import { useEffect, useRef } from 'react';
+import ReactECharts from 'echarts-for-react';
+import Chart from './components/Chart';
 
 function App() {
   const pieChartData = useSelector(state => state.expenseSlice);
-   const divRef = useRef();
-   
-   useEffect(()=>{
-    if(pieChartData.All.length >0)
-    {
-      if (divRef.current) {    
-        divRef.current.style.display = 'none';
-       
-      }
-      
-    }
-   },[pieChartData])
 
 
   return (
     <>
     <h1 className='text-center bg-dark text-light w-100 p-3 '>EXPENSES TRACKER USING REDUX-TOOLKIT</h1>
     <div className="container ">
-      
-      {/* <div className='row mt-3 '>
-        <div className='col-12 ' >
-          <h1 className='text-center bg-dark text-light '>EXPENSES TRACKER USING REDUX-TOOLKIT</h1>
-        </div>
+      {/* <div style={{height:"400px",width:"100%"}}>
+        <Chart/>
       </div> */}
+      
+    
 
 
       <div className='row mt-3'>
@@ -42,11 +31,13 @@ function App() {
         <div className='col-12 mt-5 col-md-6 mt-md-0' id='right'>
           <h4 className='text-center m-0 '>Expenses Distribution by Category</h4>
           <div className='chart' >
-          {/* <PieChart /> */}
-          {pieChartData.All.length >0 ?<PieChart /> : <div className='circle'>
+        
+          {/* {pieChartData.All.length >0 ?<PieChart /> : <div className='circle'>
             <h1>Add Expenses</h1>
             <h1>To Show Graph</h1>
-          </div>}
+          </div>} */}
+          <Chart/>
+          
          
           </div>
          
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,9 +1,129 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
+import ReactECharts from 'echarts-for-react';
+import { useSelector } from 'react-redux';
 function Chart() {
-  return (
-    <div>Chart</div>
-  )
+
+    const pieChartData = useSelector(state => state.expenseSlice);
+    console.log("pieChartData",pieChartData);
+
+    const resultObject = {};
+
+   
+    pieChartData.All.forEach(item => {
+        if (resultObject[item.Category]) {
+            resultObject[item.Category] += item.Amount;
+        } else {
+            resultObject[item.Category] = item.Amount;
+        }
+    });
+    
+    console.log(resultObject);
+    console.log(Object.keys(resultObject).map((category)=>{
+        return {value: resultObject[category] ,name:category}
+    }));
+
+    
+
+
+    const option1= {
+        series: [
+            {
+                name: 'Access From',
+                type: 'pie',
+                radius: ['40%', '70%'],
+                avoidLabelOverlap: false,
+                itemStyle: {
+                    borderRadius: 10,
+                    borderColor: '#fff',
+                    borderWidth: 2
+                },
+                label: {
+                    show: true,
+                    position: 'outside',
+                    formatter: '{b}:'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: 20,
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: true,
+                    length: 10
+                },
+                data: [
+                    { value: 1, name: 'Search Engine' },
+                    { value: 1, name: 'Direct' },
+                    { value: 1, name: 'Email' },
+                    { value: 1, name: 'Union Ads' },
+                    { value: 1, name: 'Video Ads' }
+                ]
+            }
+        ],
+        graphic: [{
+            type: 'text',
+            left: 'center',
+            top: 'middle',
+            style: {
+                text: 'Add Data To Display',
+                textAlign: 'center',
+                fill: '#333',
+                fontSize: 10
+            }
+        }]
+    };
+
+    const option2 = {
+        tooltip: {
+          trigger: 'item'
+        },
+        legend: {
+          top: '5%',
+          left: 'center'
+        },
+        series: [
+          {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            padAngle: 5,
+            itemStyle: {
+              borderRadius: 10
+            },
+            label: {
+                show: true,
+                position: 'outside',
+                formatter: '{b}: {c}'
+            },
+            emphasis: {
+              label: {
+                show: true,
+                fontSize: 40,
+                fontWeight: 'bold'
+              }
+            },
+            labelLine: {
+              show: false
+            },
+            data: Object.keys(resultObject).map((category)=>{
+                return {value: resultObject[category] ,name:category}
+            })
+          }
+        ]
+      };
+   
+
+    return (
+      
+        <ReactECharts
+        option={pieChartData.All.length > 0 ? option2 : option1}
+    />
+          
+       
+    );
 }
 
-export default Chart
+export default Chart;
